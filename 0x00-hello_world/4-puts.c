f7e50145abe47d955d0457151950eec90c931fe1
Achilik <Achilikes@gmail.com>
2022-10-13 14:13:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
-#!/bin/bash
-
-/*
-* 4-puts.c
-* alx-low_level_programming
-*/
-
 #include <stdio.h>
 
 /**
 * main - Prints "Programming is like building a multilingual
-	 puzzle, followed by a new line.
+*	 puzzle, followed by a new line.
 *
 *Return: Always 0.
 */
- int main(void)
- {
-	puts("/ "Programming is like building a multingual puzzle");
-
+int main(void)
+{
+	puts("/"Programming is like building a multingual puzzle");
 	return (0);
 }
