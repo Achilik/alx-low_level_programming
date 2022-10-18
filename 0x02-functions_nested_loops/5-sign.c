915fea3cfd0b1ff71ed8623fe33e51bd7f56cdf2
Achilik <Achilikes@gmail.com>
2022-10-18 16:20:41 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_sign - computes the absolute value of an integer
+* @n: The integer to be computed
+*
+* Return: The absolue value of the integer
+*/
+
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+
+		_putchar('-');
+		return (-n);
+	}
+}
