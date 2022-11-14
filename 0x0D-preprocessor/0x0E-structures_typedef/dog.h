24509eaa1e98aa34df6f76e766381db66a61e63f
Achilik <Achilikes@gmail.com>
2022-11-14 13:55:50 -0800

    dog.h

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,27 +1,28 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
 
-#include "main.h"
+#define DOG_H
 
 /**
- * struct dog - info on dog.
- * @name: dog's name.
- * @age: age of the dog in float.
- * @owner: name of the owner of the dog.
+ * struct dog - a dog
  *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
 
+} dog;
+
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/* function for typedef dog_t */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _putchar(char c);
+
 #endif
