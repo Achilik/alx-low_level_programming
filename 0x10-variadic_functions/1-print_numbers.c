0fd3018d633dbd75944869cce197a576e2e37785
Achilik <Achilikes@gmail.com>
2022-11-17 07:09:30 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,30 +3,26 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - print given string
- * @separator: separator to print with if Null don't print any separators.
- * @n: number of arguments to print.
- * @...: string to print.
+ * print_numbers - this function will print the number passes to it.
+ * @separator: element to print as the separator.
+ * @n: number of elements to print.
+ * @...: arguments to print.
  *
- * Return: Nothing.
+ * Return: Nothing
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list vls;
-	unsigned int i;
-	char *r;
+	unsigned int i = 0;
 
 	va_start(vls, n);
-	i = 0;
+
 	while (i < n)
 	{
-		r = va_arg(vls, char*);
-		if (!r)
-			printf("(nil)");
-		else
-			printf("%s", r);
+
+		printf("%d", va_arg(vls, int));
 		i++;
-		if (i < n && separator)
+		if (i < n && (separator))
 			printf("%s", separator);
 	}
 	putchar(10);
